8482aa5c082581cffa3ee3ee3211e567b6a87987
hatem <hatem20@github.com>
2018-06-13 22:49:02 +0200

    add SensioGeneratorBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test', 'test_cached'], true)) {
             $bundles[] = new \Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle();
             $bundles[] = new \Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle();
+            $bundles[] = new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return array_merge(parent::registerBundles(), $bundles);
---
+++ src/AppBundle/Menu/AdminMenuListener.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hatem
+ * Date: 6/12/18
+ * Time: 6:40 PM
+ */
+
+namespace AppBundle\Menu;
+
+
+class AdminMenuListener
+{
+
+}
