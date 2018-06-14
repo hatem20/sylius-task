0ceef3b1b3eaf8e939f705759300e201b630c035
hatem <hatem20@github.com>
2018-06-14 14:23:42 +0200

    disable autowiring for ResourceController

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -15,35 +15,35 @@ class ProductController extends ResourceController
      *
      * @return Response
      */
-    public function showAction(Request $request): Response
-    {
-        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
-
-        $this->isGrantedOr403($configuration, ResourceActions::SHOW);
-        $product = $this->findOr404($configuration);
-
-        $recommendationServiceApi = $this->get('app.recommendation_service_api');
-
-        $recommendedProducts = $recommendationServiceApi->getRecommendedProducts($product);
-
-        $this->eventDispatcher->dispatch(ResourceActions::SHOW, $configuration, $product);
-
-        $view = View::create($product);
-
-        if ($configuration->isHtmlRequest()) {
-            $view
-                ->setTemplate($configuration->getTemplate(ResourceActions::SHOW . '.html'))
-                ->setTemplateVar($this->metadata->getName())
-                ->setData([
-                    'configuration' => $configuration,
-                    'metadata' => $this->metadata,
-                    'resource' => $product,
-                    'recommendedProducts' => $recommendedProducts,
-                    $this->metadata->getName() => $product,
-                ])
-            ;
-        }
-
-        return $this->viewHandler->handle($configuration, $view);
-    }
+//    public function showAction(Request $request): Response
+//    {
+//        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+//
+//        $this->isGrantedOr403($configuration, ResourceActions::SHOW);
+//        $product = $this->findOr404($configuration);
+//
+//        $recommendationServiceApi = $this->get('app.recommendation_service_api');
+//
+//        $recommendedProducts = $recommendationServiceApi->getRecommendedProducts($product);
+//
+//        $this->eventDispatcher->dispatch(ResourceActions::SHOW, $configuration, $product);
+//
+//        $view = View::create($product);
+//
+//        if ($configuration->isHtmlRequest()) {
+//            $view
+//                ->setTemplate($configuration->getTemplate(ResourceActions::SHOW . '.html'))
+//                ->setTemplateVar($this->metadata->getName())
+//                ->setData([
+//                    'configuration' => $configuration,
+//                    'metadata' => $this->metadata,
+//                    'resource' => $product,
+//                    'recommendedProducts' => $recommendedProducts,
+//                    $this->metadata->getName() => $product,
+//                ])
+//            ;
+//        }
+//
+//        return $this->viewHandler->handle($configuration, $view);
+//    }
 }
